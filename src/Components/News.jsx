79591740d501ecb3b7937dc1e7ba28d1e7c65458
Anubhav Tripathi <anubhavtripathi0509@gmail.com>
2024-09-09 13:03:07 +0530

    commit

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -27,9 +27,11 @@ const News = () => {
     // Loading state
     if (loading) {
         return (
-            <div className="relative flex justify-center items-center">
-                <div className="absolute animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-purple-500"></div>
-                <img src="https://www.svgrepo.com/show/509001/avatar-thinking-9.svg" className="rounded-full h-28 w-28" alt="Loading avatar" />
+            <div className="flex justify-center items-center min-h-screen">
+                <div className="relative">
+                    <div className="absolute animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-purple-500"></div>
+                    <img src="https://www.svgrepo.com/show/509001/avatar-thinking-9.svg" className="rounded-full h-28 w-28" alt="Loading avatar" />
+                </div>
             </div>
         );
     }
